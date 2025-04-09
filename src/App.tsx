461bfaa87dc74c31cb3004cf17a23b8e2d3e3c57
Tuan <leviettuan28052003@gmail.com>
2025-04-09 23:03:47 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      Hello world!
+      <h2 className="text-2xl font-medium text-red-100">Xin chào</h2>
+      <div className="rounded bg-blue-500 px-4 py-2 text-center font-bold text-white hover:bg-blue-600">
+        Click me
+      </div>
+    </h1>
+  );
+}
+
+export default App;
